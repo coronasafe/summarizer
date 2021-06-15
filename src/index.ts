e4ee3a5fb161117862a09a97aeef6073fa3b228a
Karan Sanjeev <yo@alphaman.me>
2021-06-15 13:31:15 +0530

    Get PRs made today

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+import * as moment from "moment";
+import * as github from "@actions/github";
+
+const main = async () => {
+  const octo = github.getOctokit(process.env.DISCUSSION_TOKEN);
+  const cfePullRequests = await octo.rest.pulls.list({
+    repo: "care_fe",
+    owner: "coronasafe",
+  });
+
+  // All PRs made today at coronasafe/care_fe
+  const thePRS = cfePullRequests.data.filter((prs) =>
+    moment(prs.created_at).isSame(moment(), "date")
+  );
+
+  return thePRS;
+};
+
+main().catch(console.error);

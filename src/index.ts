e2270646647bc390643ea868ff6ac50383c7bb64
Karan Sanjeev <yo@alphaman.me>
2021-06-15 13:44:47 +0530

    Get list of users in CEF team

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,24 @@ import * as github from "@actions/github";
 
 const main = async () => {
   const octo = github.getOctokit(process.env.DISCUSSION_TOKEN);
+  const cefStudents = await octo.rest.teams.listMembersInOrg({
+    org: "coronasafe",
+    team_slug: "cef",
+  });
   const cfePullRequests = await octo.rest.pulls.list({
     repo: "care_fe",
     owner: "coronasafe",
   });
+
+  // All members inside the CEF team
+  const students = cefStudents.data.map((student) => student.login);
 
   // All PRs made today at coronasafe/care_fe
   const thePRS = cfePullRequests.data.filter((prs) =>
     moment(prs.created_at).isSame(moment(), "date")
   );
 
-  return thePRS;
+  return { thePRS, students };
 };
 
 main().catch(console.error);

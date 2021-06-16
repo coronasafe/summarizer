db9daec06935ddc56de21e50823b05c5aecbdefc
Karan Sanjeev <yo@alphaman.me>
2021-06-16 20:42:05 +0530

    Write to JSON file

--- packages/dasaver/src/index.ts
+++ packages/dasaver/src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import * as moment from "moment";
 import DAFetch from "@coronasafe/dafetch";
+import * as writeJsonFile from "write-json-file";
 
 dotenv.config();
 const repos = ["care_fe", "life", "dashboard", "care", "citizen", "arike"];
@@ -12,12 +13,34 @@ const main = async () => {
     "cef"
   );
   const members = await dafetch.getTeamMembers();
-  const pulls = await Promise.all(
+  let pulls = await Promise.all(
     repos.map(async (repo) => await dafetch.getPullsForRepoToday(repo))
   );
-  const reviews = await Promise.all(
+  let reviews = await Promise.all(
     repos.map(async (repo) => await dafetch.getReviewsForRepoToday(repo))
   );
+
+  // Return only the PRs made by team members
+  pulls = pulls.map((repos) =>
+    repos.filter((pull) => members.includes(pull.author))
+  );
+  // Return only the reviews made by team members
+  reviews = reviews.map((repos) =>
+    repos.filter((review) =>
+      review.reviewedBy.some((reviewer) => members.includes(reviewer))
+    )
+  );
+
+  const date = moment().format("DD-MM-YYYY");
+  const membersJson = date + "/members.json";
+  const reviewsJson = date + "/reviews.json";
+  const pullsJson = date + "/pulls.json";
+
+  await Promise.all([
+    writeJsonFile(membersJson, members),
+    writeJsonFile(reviewsJson, reviews),
+    writeJsonFile(pullsJson, pulls),
+  ]);
 
   return { members, pulls, reviews };
 };

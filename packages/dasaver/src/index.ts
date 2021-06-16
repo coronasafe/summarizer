08d0b7af15e5f9b0279abb87cd38fbee0b38eb6a
Karan Sanjeev <yo@alphaman.me>
2021-06-16 19:35:26 +0530

    daSaver packager

---
+++ packages/dasaver/src/index.ts
@@ -0,0 +1,35 @@
+import * as dotenv from "dotenv";
+import * as moment from "moment";
+import DAFetch from "@coronasafe/dafetch";
+
+dotenv.config();
+const repos = ["care_fe", "life", "care", "citizen", "arike"];
+
+const main = async () => {
+  const dafetch = new DAFetch(
+    process.env.DISCUSSION_TOKEN,
+    "coronasafe",
+    "cef"
+  );
+  const members = await dafetch.getTeamMembers();
+  const pulls = await Promise.all(
+    repos.map(
+      async (repo) =>
+        await dafetch.getPullsForRepoBetween(
+          repo,
+          moment().subtract(1),
+          moment()
+        )
+    )
+  );
+  const reviews = await Promise.all(
+    repos.map(
+      async (repo) =>
+        await dafetch.getReviewsForRepo(repo, moment().subtract(1), moment())
+    )
+  );
+
+  return { members, pulls, reviews };
+};
+
+main().then(console.log).catch(console.error);
